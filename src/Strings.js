359e1d664266d6ffeef79b5cac9d461c93523c95
Matthew Skillington <matthew.skillington@ideagenplc.com>
2020-01-17 13:55:09 +0000

    Adding localisation

--- src/Strings.js
+++ src/Strings.js
@@ -1,13 +1,28 @@
-const strings = {
-    LoginSSOMessage: "Log in through SSO",
-    ssoButtonTitle: "Ideagen365Test",
-    adfsLogin: "ADFS Login",
-    rememberMe: "Remember me",
-    orLabel: "or",
-    loginCoruson: "Log in through Coruson Account",
-    usernameLabel: "Username/Email Address:",
-    passwordLabel: "Password:",
-    loginButton: "LOGIN"
-}
+import LocalizedStrings from 'react-native-localization';
+
+let strings = new LocalizedStrings({
+    "en-US": {
+        LoginSSOMessage: "Log in through SSO",
+        ssoButtonTitle: "Ideagen365Test",
+        adfsLogin: "ADFS Login",
+        rememberMe: "Remember me",
+        orLabel: "or",
+        loginCoruson: "Log in through Coruson Account",
+        usernameLabel: "Username/Email Address:",
+        passwordLabel: "Password:",
+        loginButton: "LOGIN"
+    },
+    "es": {
+        LoginSSOMessage: "Inicie sesión a través de SSO",
+        ssoButtonTitle: "Ideagen365Test",
+        adfsLogin: "ADFS Iniciar sesión",
+        rememberMe: "Recuérdame",
+        orLabel: "o",
+        loginCoruson: "Inicie sesión a través de la cuenta Coruson",
+        usernameLabel: "Nombre de usuario",
+        passwordLabel: "Contraseña",
+        loginButton: "Iniciar sesión"
+    }
+});
 
 export default strings;
